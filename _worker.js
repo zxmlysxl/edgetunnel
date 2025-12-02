716386473177a6fe6b9dc32da8db36395c6fbeaa
cmliu <cmliu@users.noreply.github.com>
2025-12-02 16:47:34 +0800

    fix: 默认禁用0RTT以增强兼容性

--- _worker.js
+++ _worker.js
@@ -850,7 +850,7 @@ async function 读取config_JSON(env, hostname, userID, 重置配置 = false) {
         协议类型: "v" + "le" + "ss",
         传输协议: "ws",
         跳过证书验证: true,
-        启用0RTT: true,
+        启用0RTT: false,
         TLS分片: null,
         随机路径: false,
         优选订阅生成: {
